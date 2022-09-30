eaca8c603b3304478b1e04aabf98563eaeaf3e88
P-BUS <pavlo.buslowski@gmail.com>
2022-09-30 15:40:06 +0300

    Started to add DataStore to save choosed layout type

--- app/src/main/java/com/example/arti/data/SettingsDataStore.kt
+++ app/src/main/java/com/example/arti/data/SettingsDataStore.kt
@@ -1,8 +1,38 @@
 package com.example.arti.data
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+
+private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
+
+private val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
+    name = LAYOUT_PREFERENCES_NAME
+)
 
 class SettingsDataStore(context: Context) {
 
+    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+
+    val preferenceFlow: Flow<Boolean> = context.dataStore.data
+        .catch {
+
+        }
+        .map { preferences ->
+            // On the first run of the app, we will use LinearLayoutManager by default
+            preferences[IS_LINEAR_LAYOUT_MANAGER] ?: true
+        }
+
+    suspend fun saveLayoutToPreferencesStore(isLinearLayoutManager: Boolean, context: Context) {
+        context.dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
 
 }
