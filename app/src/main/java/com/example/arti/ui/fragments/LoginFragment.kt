8b8034211f2a8066362100f20eec61c8bf2449e2
P-BUS <pavlo.buslowski@gmail.com>
2023-01-01 16:59:01 +0200

    added methods for email auth

--- app/src/main/java/com/example/arti/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/arti/ui/fragments/LoginFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.example.arti.R
 import com.example.arti.databinding.LoginFragmentBinding
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
 import com.google.firebase.auth.FirebaseAuth
@@ -40,7 +39,8 @@ class LoginFragment : Fragment() {
         auth = Firebase.auth
 
         binding.login.setOnClickListener { startSignIn() }
-           }
+
+    }
 
     public override fun onStart() {
         super.onStart()
@@ -52,8 +52,68 @@ class LoginFragment : Fragment() {
         }
     }
 
+    fun createAccount(email: String, password: String) {
+        if (!validateForm()) {
+            return
+        }
+        showProgressBar()
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(context, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+                hideProgressBar()
+            }
+    }
 
+    fun signIn(email: String, password: String) {
+        if (!validateForm()) {
+            return
+        }
+        showProgressBar()
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(context, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+                hideProgressBar()
+            }
+    }
 
+    fun getCurrentUser() {
+        val user = Firebase.auth.currentUser
+        user?.let {
+            // Name, email address, and profile photo Url
+            val name = user.displayName
+            val email = user.email
+            val photoUrl = user.photoUrl
 
+            // Check if user's email is verified
+            val emailVerified = user.isEmailVerified
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            val userId = user.uid
+        }
+    }
 
 }
