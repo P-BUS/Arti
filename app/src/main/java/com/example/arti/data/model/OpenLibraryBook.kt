a44d287873e73a8c747e1db36a62740ffa16d6d3
P-BUS <pavlo.buslowski@gmail.com>
2023-02-20 18:42:32 +0200

    change naming to camel case using serializedName

--- app/src/main/java/com/example/arti/data/model/OpenLibraryBook.kt
+++ app/src/main/java/com/example/arti/data/model/OpenLibraryBook.kt
@@ -1,37 +1,60 @@
 package com.example.arti.data.model
 
 import com.example.arti.utils.Constants
+import com.google.gson.annotations.SerializedName
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class OpenLibraryBook(
-    val _version_: Long = Constants.MISSED_LONG_DATA,
-    val author_alternative_name: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val author_facet: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val author_key: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val author_name: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val cover_edition_key: String = Constants.MISSED_STRING_DATA,
-    val cover_i: Int = Constants.MISSED_INT_DATA,
-    val ebook_access: String = Constants.MISSED_STRING_DATA,
-    val ebook_count_i: Int = Constants.MISSED_INT_DATA,
-    val edition_count: Int = Constants.MISSED_INT_DATA,
-    val edition_key: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val first_publish_year: Int = Constants.MISSED_INT_DATA,
-    val has_fulltext: Boolean = Constants.MISSED_BOOLEAN_DATA,
+    @SerializedName("_version_")
+    val version: Long = Constants.MISSED_LONG_DATA,
+    @SerializedName("author_alternative_name")
+    val authorAlternativeName: List<String> = listOf(Constants.MISSED_STRING_DATA),
+    @SerializedName("author_facet")
+    val authorFacet: List<String> = listOf(Constants.MISSED_STRING_DATA),
+    @SerializedName("author_key")
+    val authorKey: List<String> = listOf(Constants.MISSED_STRING_DATA),
+    @SerializedName("author_name")
+    val authorName: List<String> = listOf(Constants.MISSED_STRING_DATA),
+    @SerializedName("cover_edition_key")
+    val coverEditionKey: String = Constants.MISSED_STRING_DATA,
+    @SerializedName("cover_i")
+    val coverI: Int = Constants.MISSED_INT_DATA,
+    @SerializedName("ebook_access")
+    val ebookAccess: String = Constants.MISSED_STRING_DATA,
+    @SerializedName("ebook_count_i")
+    val ebookCountI: Int = Constants.MISSED_INT_DATA,
+    @SerializedName("edition_count")
+    val editionCount: Int = Constants.MISSED_INT_DATA,
+    @SerializedName("edition_key")
+    val editionKey: List<String> = listOf(Constants.MISSED_STRING_DATA),
+    @SerializedName("first_publish_year")
+    val firstPublishYear: Int = Constants.MISSED_INT_DATA,
+    @SerializedName("has_fulltext")
+    val hasFulltext: Boolean = Constants.MISSED_BOOLEAN_DATA,
     val ia: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val ia_collection_s: String = Constants.MISSED_STRING_DATA,
+    @SerializedName("ia_collection_s")
+    val iaCollectionS: String = Constants.MISSED_STRING_DATA,
     val key: String = Constants.MISSED_STRING_DATA,
     val language: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val last_modified_i: Int = Constants.MISSED_INT_DATA,
+    @SerializedName("last_modified_i")
+    val lastModifiedI: Int = Constants.MISSED_INT_DATA,
+    @SerializedName("publicScanB")
     val public_scan_b: Boolean = Constants.MISSED_BOOLEAN_DATA,
-    val publish_date: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val publish_year: List<Int> = listOf(Constants.MISSED_INT_DATA),
+    @SerializedName("publish_date")
+    val publishDate: List<String> = listOf(Constants.MISSED_STRING_DATA),
+    @SerializedName("publish_year")
+    val publishYear: List<Int> = listOf(Constants.MISSED_INT_DATA),
+    @SerializedName("total_count")
     val seed: List<String> = listOf(Constants.MISSED_STRING_DATA),
     val subject: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val subject_facet: List<String> = listOf(Constants.MISSED_STRING_DATA),
-    val subject_key: List<String> = listOf(Constants.MISSED_STRING_DATA),
+    @SerializedName("subject_facet")
+    val subjectFacet: List<String> = listOf(Constants.MISSED_STRING_DATA),
+    @SerializedName("subject_key")
+    val subjectKey: List<String> = listOf(Constants.MISSED_STRING_DATA),
     val title: String = Constants.MISSED_STRING_DATA,
-    val title_suggest: String = Constants.MISSED_STRING_DATA,
+    @SerializedName("title_suggest")
+    val titleSuggest: String = Constants.MISSED_STRING_DATA,
     val type: String = Constants.MISSED_STRING_DATA
 )
 
--- app/src/main/java/com/example/arti/data/model/OpenLibrarySearchResponse.kt
+++ app/src/main/java/com/example/arti/data/model/OpenLibrarySearchResponse.kt
@@ -1,16 +1,15 @@
 package com.example.arti.data.model
 
+import com.google.gson.annotations.SerializedName
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class OpenLibrarySearchResponse(
-    val docs: List<OpenLibraryBook>,
-    val numFound: Int,
-    //val numFoundExact: Boolean,
-    val num_found: Int,
-    //val offset: Any,
-    val q: String,
-    val start: Int
+    val docs: List<OpenLibraryBook> = listOf(),
+    @SerializedName("num_found")
+    val numFound: Int = 0,
+    val q: String = "",
+    val start: Int = 0
 )
 
 
