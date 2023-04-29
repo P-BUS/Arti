e1808cda84a2db001bc486b016068ca4db4f6da8
P-BUS <pavlo.buslowski@gmail.com>
2023-04-29 18:41:53 +0300

    made splash theme in separate file

--- app/src/main/java/com/example/arti/MainActivity.kt
+++ app/src/main/java/com/example/arti/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.arti
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         // Retrieve NavController from the NavHostFragment
